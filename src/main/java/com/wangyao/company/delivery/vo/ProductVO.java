872c06d024c5a0ace08529d625d08d8aab79bb89
wangyao11 <657316992@qq.com>
2019-12-23 14:06:21 +0800

    修改产品定义错误问题

--- src/main/java/com/wangyao/company/delivery/vo/ProductVO.java
+++ src/main/java/com/wangyao/company/delivery/vo/ProductVO.java
@@ -19,12 +19,58 @@ import java.util.Date;
 @AllArgsConstructor
 public class ProductVO {
     /**
-    * 产品销量
-    */
+     * 产品Id
+     */
+    private Long id;
+
+    /**
+     * 产品名称
+     */
+    private String name;
+
+    /**
+     * 产品图片
+     */
+    private String imageUrl;
+
+    /**
+     * 分类Id
+     */
+    private Long classId;
+
+    /**
+     * 备注
+     */
+    private String remark;
+
+    private float price;
+
+    /**
+     * 状态:0-上架,1-下架
+     */
+    private Byte states;
+
+    /**
+     * 商品类型 0-斤 1-袋 2-瓶 3-桶 4-包
+     */
+    private String type;
+
+    /**
+     * 分类排序，从大到小
+     */
+    private Integer sort;
+
     private int count;
 
     /**
-    * 产品名称
-    */
-    private String productName;
+     * 记录创建时间
+     */
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    private Date createTime;
+
+    /**
+     * 记录更新时间
+     */
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    private Date updateTime;
 }
